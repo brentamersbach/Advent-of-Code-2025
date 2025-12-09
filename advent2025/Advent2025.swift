31b6e5a7ba59e55ef4c36fd0c642be4ab4441ae7
Brent Amersbach <triumshockwave@gmail.com>
2025-12-09 12:53:47 -0500

    Day 2 part 1 done

---
+++ advent2025/Advent2025.swift
@@ -0,0 +1,24 @@
+//
+//  Advent2025.swift
+//  advent2025
+//
+//  Created by Brent Amersbach on 12/4/25.
+//
+
+@main struct Advent2025 {
+    static func main() throws {
+        var days: [Day] = []
+        
+        let day1=Day_1()
+        days.append(day1)
+        
+        let day2=Day_2()
+        days.append(day2)
+        
+        for day in days {
+            print("Day \(day.num)\n")
+            day.run()
+            print("\n------------\n")
+        }
+    }
+}
---
+++ advent2025/Day.swift
@@ -0,0 +1,18 @@
+//
+//  Untitled.swift
+//  advent2025
+//
+//  Created by Brent Amersbach on 12/4/25.
+//
+
+class Day {
+    let num: Int
+    
+    init(num: Int) {
+        self.num = num
+    }
+    
+    func run() {
+        return
+    }
+}
